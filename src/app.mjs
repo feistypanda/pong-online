98dbc750e2134a118fac45e973bbb29858dac0a7
Leo Milberg <milbergleo@gmail.com>
2026-08-22 22:23:32 -0400

    form input working

--- src/app.mjs
+++ src/app.mjs
@@ -1,26 +1,47 @@
 
-
 import path from 'path';
 
 import ejs from 'ejs';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import { closeDatabaseConnection, connectToDatabase, getCollection } from "./db.mjs";
 
 const port = 8000;
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', '../public/views');
 
 app.get('/', async (req, res) => {
 
-	const users = getCollection("users");
-	const user = await users.findOne({"username": "leo"});
+	const usersCollection = getCollection("users");
+	const cursor = await usersCollection.find({"username": "leo"});
+	const documents = await cursor.toArray()
 
-	res.render('index', { title: "index", message: user.username });
+	res.render('index', { "title": "index", "users": documents});
+});
+
+app.get('/login', async (req, res) => {
+	res.render('login', { title: "login" });
+});
+
+app.get('/register', async (req, res) => {
+	res.render('register', { title: "register" });
+});
+
+app.post('/register', (req, res) => {
+	res.render('register', { title: req.body.username });
+});
+
+app.get('/play', async (req, res) => {
+	res.render('play', { title: "play" });
+});
+
+app.get('/stats', async (req, res) => {
+	res.render('stats', { title: "stats" });
 });
 
 async function startServer () {
